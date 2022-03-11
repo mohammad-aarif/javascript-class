61dd9e041c0ef4584a2ab855310b7a621dfa6eb6
mohammad-aarif <arifbd354@gmail.com>
2022-03-11 11:28:32 +0600

    first push

---
+++ firstClass.js
@@ -0,0 +1,15 @@
+// variable 
+// var name = 'arif';
+// console.log(name);
+
+// String 
+// Number
+// Boolean
+// Array 
+// Object
+
+var dist = "Dhaka"
+var age = 12
+var isRaining = false
+
+console.log(isRaining)
